1dcef62e17be4b491a22f34185fa1f64cc10ab63
alexsotocx <asoto@innventto.com>
2015-02-19 18:50:30 -0500

    Clase 3

--- Clase1/app/src/main/java/com/robotica/clase1/MainActivity.java
+++ Clase1/app/src/main/java/com/robotica/clase1/MainActivity.java
@@ -2,39 +2,39 @@ package com.robotica.clase1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements CompoundButton.OnCheckedChangeListener {
 
-	private Button mButtonDisconnect;
-	private Button mButtonConnect;
 	private TextView mTextConnectionStatus;
+	private Switch mSwitch1;
 	private TextView mTextProgress;
+	private TextView mTextProgress2;
 	private SeekBar mSeekBar;
-
+	private SeekBar mSeekBar2;
+	private Switch mSwitch2;
+	private Switch mSwitch3;
+	private TextView hexText;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		mButtonDisconnect = (Button) findViewById(R.id.button_connect);
-		mButtonConnect = (Button) findViewById(R.id.button_disconnect);
 		mTextConnectionStatus = (TextView) findViewById(R.id.connection_status_text);
 		mTextProgress = (TextView) findViewById(R.id.progress1);
+		mTextProgress2 = (TextView) findViewById(R.id.progress2);
 		mSeekBar = (SeekBar) findViewById(R.id.slider_1);
-		mButtonDisconnect.setOnClickListener(new clickListener());
-		mButtonConnect.setOnClickListener(new clickListener());
 		mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				mTextProgress.setText("" + progress);
+				updateHexText();
 			}
 
 			@Override
@@ -47,6 +47,35 @@ public class MainActivity extends ActionBarActivity {
 
 			}
 		});
+		mSeekBar2 = (SeekBar) findViewById(R.id.slider_2);
+		mSeekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				mTextProgress2.setText("" + progress);
+				updateHexText();
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+
+			}
+		});
+
+
+
+		mSwitch1 = (Switch) findViewById(R.id.switch1);
+		mSwitch2 = (Switch) findViewById(R.id.switch2);
+		mSwitch3 = (Switch) findViewById(R.id.switch3);
+		mSwitch1.setOnCheckedChangeListener(this);
+		mSwitch2.setOnCheckedChangeListener(this);
+		mSwitch3.setOnCheckedChangeListener(this);
+
+		hexText = (TextView) findViewById(R.id.hex_text);
 	}
 
 
@@ -73,19 +102,22 @@ public class MainActivity extends ActionBarActivity {
 	}
 
 
-	private class clickListener implements View.OnClickListener{
 
-		@Override
-		public void onClick(View v) {
-			switch (v.getId()){
-				case R.id.button_disconnect:
-					mTextConnectionStatus.setText(getString(R.string.off));
-					break;
-				case R.id.button_connect:
-					mTextConnectionStatus.setText(getString(R.string.on));
-					break;
-			}
+	private void updateHexText(){
+		String t2 = mSwitch2.isChecked()? "01" : "00";
+		String t3 = mSwitch3.isChecked()? "01" : "00";
+		String s1 = (mSeekBar.getProgress() < 16 ? "0":"") +  Integer.toHexString(mSeekBar.getProgress());
+		String s2 = (mSeekBar2.getProgress() < 16 ? "0":"") +  Integer.toHexString(mSeekBar2.getProgress());
+		hexText.setText(("7E  " + t2 + "  " + t3 + "  " + s1 + "  " + s2 + "  00").toUpperCase() );
+	}
 
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		switch (buttonView.getId()){
+			case R.id.switch1:
+				mTextConnectionStatus.setText(isChecked? getString(R.string.on) : getString(R.string.off));
 		}
+		updateHexText();
+
 	}
 }
