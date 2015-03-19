86fd56ec643bda174db83c14fe7ac9d4b1fabffa
alexsotocx <asoto@innventto.com>
2015-03-18 22:54:42 -0500

    Clase 3

--- Clase1/app/src/main/java/com/robotica/clase1/MainActivity.java
+++ Clase1/app/src/main/java/com/robotica/clase1/MainActivity.java
@@ -9,8 +9,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
@@ -20,211 +18,209 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity implements CompoundButton.OnCheckedChangeListener, SensorEventListener {
 
-	private TextView mTextConnectionStatus;
-	private Switch mSwitch1;
-	private TextView mTextIp;
-	private TextView mTextProgress;
-	private TextView mTextProgress2;
-	private SeekBar mSeekBar;
-	private SeekBar mSeekBar2;
-	private Switch mSwitch2;
-	private Switch mSwitch3;
-	private Switch mSwitchAccelerometers;
-	private TextView hexText;
-	//acelerometro
-	private SensorManager sensorManager;
-	private Sensor accelerometer;
-	private boolean isAccelerometerActive = false;
-	private boolean isConnected = false;
-	private long lastUpdate;
+  private TextView mTextConnectionStatus;
+  private Switch mSwitch1;
+  private TextView mTextIp;
+  private TextView mTextProgress;
+  private TextView mTextProgress2;
+  private SeekBar mSeekBar;
+  private SeekBar mSeekBar2;
+  private Switch mSwitch2;
+  private Switch mSwitch3;
+  private Switch mSwitchAccelerometers;
+  private TextView hexText;
+  //acelerometro
+  private SensorManager sensorManager;
+  private Sensor accelerometer;
+  private boolean isAccelerometerActive = false;
+  private boolean isConnected = false;
+  private long lastUpdate;
 
-	private TCPConnection connection;
-
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		mTextConnectionStatus = (TextView) findViewById(R.id.connection_status_text);
-		mTextProgress = (TextView) findViewById(R.id.progress1);
-		mTextIp = (TextView) findViewById(R.id.ip_address);
-		mTextProgress2 = (TextView) findViewById(R.id.progress2);
-		mSeekBar = (SeekBar) findViewById(R.id.slider_1);
-		mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-				mTextProgress.setText("" + progress);
-				updateHexText();
-			}
-
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-
-			}
-
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-
-			}
-		});
-		mSeekBar2 = (SeekBar) findViewById(R.id.slider_2);
-		mSeekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-				mTextProgress2.setText("" + progress);
-				updateHexText();
-			}
-
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-
-			}
-
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-
-			}
-		});
-		mSwitch1 = (Switch) findViewById(R.id.switch1);
-		mSwitch2 = (Switch) findViewById(R.id.switch2);
-		mSwitch3 = (Switch) findViewById(R.id.switch3);
-		mSwitchAccelerometers = (Switch) findViewById(R.id.switch_acceloremter);
-		mSwitch1.setOnCheckedChangeListener(this);
-		mSwitch2.setOnCheckedChangeListener(this);
-		mSwitch3.setOnCheckedChangeListener(this);
-		mSwitchAccelerometers.setOnCheckedChangeListener(this);
-
-		hexText = (TextView) findViewById(R.id.hex_text);
-
-		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-			// success! we have an accelerometer
-
-			accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-			sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
-		} else {
-			// fail! we don't have an accelerometer!
-		}
-
-	}
+  private TCPConnection connection;
 
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    mTextConnectionStatus = (TextView) findViewById(R.id.connection_status_text);
+    mTextProgress = (TextView) findViewById(R.id.progress1);
+    mTextIp = (TextView) findViewById(R.id.ip_address);
+    mTextProgress2 = (TextView) findViewById(R.id.progress2);
+    mSeekBar = (SeekBar) findViewById(R.id.slider_1);
+    mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        mTextProgress.setText("" + progress);
+        updateHexText();
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+    mSeekBar2 = (SeekBar) findViewById(R.id.slider_2);
+    mSeekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        mTextProgress2.setText("" + progress);
+        updateHexText();
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+    mSwitch1 = (Switch) findViewById(R.id.switch1);
+    mSwitch2 = (Switch) findViewById(R.id.switch2);
+    mSwitch3 = (Switch) findViewById(R.id.switch3);
+    mSwitchAccelerometers = (Switch) findViewById(R.id.switch_acceloremter);
+    mSwitch1.setOnCheckedChangeListener(this);
+    mSwitch2.setOnCheckedChangeListener(this);
+    mSwitch3.setOnCheckedChangeListener(this);
+    mSwitchAccelerometers.setOnCheckedChangeListener(this);
+
+    hexText = (TextView) findViewById(R.id.hex_text);
+
+    sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+      // success! we have an accelerometer
+
+      accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+      sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    } else {
+      // fail! we don't have an accelerometer!
+    }
+
+  }
 
 
-	private void updateHexText() {
-		String t2 = mSwitch2.isChecked() ? "01" : "00";
-		String t3 = mSwitch3.isChecked() ? "01" : "00";
-		String s1 = (mSeekBar.getProgress() < 16 ? "0" : "") + Integer.toHexString(mSeekBar.getProgress());
-		String s2 = (mSeekBar2.getProgress() < 16 ? "0" : "") + Integer.toHexString(mSeekBar2.getProgress());
-		String current = ("7E" + t2 + t3 + s1 + s2 + "00").toUpperCase();
-		hexText.setText(current);
-		try {
-			if (connection != null) {
-				mTextConnectionStatus.setText(connection.client.conection());
-				connection.client.sendMessage(current);
-			}
-		} catch (Exception e) {
-			//mTextConnectionStatus.setText("Error");
-		}
+  private void updateHexText() {
+    String t2 = mSwitch2.isChecked() ? "01" : "00";
+    String t3 = mSwitch3.isChecked() ? "01" : "00";
+    String s1 = (mSeekBar.getProgress() < 16 ? "0" : "") + Integer.toHexString(mSeekBar.getProgress());
+    String s2 = (mSeekBar2.getProgress() < 16 ? "0" : "") + Integer.toHexString(mSeekBar2.getProgress());
+    String current = ("7E" + t2 + t3 + s1 + s2 + "00").toUpperCase();
+    hexText.setText(current);
+    try {
+      if (connection != null) {
+        mTextConnectionStatus.setText(connection.client.conection());
+        connection.client.sendMessage(current);
+      }
+    } catch (Exception e) {
+      //mTextConnectionStatus.setText("Error");
+    }
 
-	}
+  }
 
-	@Override
-	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		switch (buttonView.getId()) {
-			case R.id.switch1:
-				if (isChecked) {
-					try {
-						connection = new TCPConnection(mTextIp.getText().toString());
-						connection.execute("");
-						mTextConnectionStatus.setText("En espera");
-					} catch (Exception e) {
-						mSwitch1.setChecked(false);
-						mTextConnectionStatus.setText("Error");
-					}
-				} else {
-					try {
-						mTextConnectionStatus.setText(connection.client.stopClient());
-					} catch (Throwable throwable){
-						throwable.printStackTrace();
-					}
-				}
-				break;
-			case R.id.switch_acceloremter:
-				isAccelerometerActive = isChecked;
-				break;
-		}
-		if(buttonView.getId() != R.id.switch1)
-			updateHexText();
+  @Override
+  public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+    switch (buttonView.getId()) {
+      case R.id.switch1:
+        if (isChecked) {
+          try {
+            connection = new TCPConnection(mTextIp.getText().toString());
+            connection.execute("");
+            mTextConnectionStatus.setText("En espera");
+          } catch (Exception e) {
+            mSwitch1.setChecked(false);
+            mTextConnectionStatus.setText("Error");
+          }
+        } else {
+          try {
+            mTextConnectionStatus.setText(connection.client.stopClient());
+          } catch (Throwable throwable) {
+            throwable.printStackTrace();
+          }
+        }
+        break;
+      case R.id.switch_acceloremter:
+        isAccelerometerActive = isChecked;
+        break;
+    }
+    if (buttonView.getId() != R.id.switch1)
+      updateHexText();
 
-	}
+  }
 
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		Sensor sensor = event.sensor;
-		if (sensor.getType() == Sensor.TYPE_ACCELEROMETER && isAccelerometerActive) {
-			float x = event.values[0];
-			float y = event.values[1];
-			float z = event.values[2];
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    Sensor sensor = event.sensor;
+    if (sensor.getType() == Sensor.TYPE_ACCELEROMETER && isAccelerometerActive) {
+      float x = event.values[0];
+      float y = event.values[1];
+      float z = event.values[2];
 
-			long currentTime = System.currentTimeMillis();
-			if ((currentTime - lastUpdate) > 300) {
-				lastUpdate = currentTime;
-				mSeekBar.setProgress((int) (z * 10));
-				mSeekBar2.setProgress((int) (y * 5 + 50));
+      long currentTime = System.currentTimeMillis();
+      if ((currentTime - lastUpdate) > 300) {
+        lastUpdate = currentTime;
+        mSeekBar.setProgress((int) (z * 10));
+        mSeekBar2.setProgress((int) (y * 5 + 50));
 
-			}
-
-
-		}
-
-	}
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-	}
+      }
 
 
-	private class TCPConnection extends AsyncTask<String, String, TCPClient> implements TCPClient.OnMessageReceived {
-		private String ipAddress;
-		private TCPClient client;
+    }
 
-		public TCPConnection(String ipAddress) {
-			this.ipAddress = ipAddress;
-		}
+  }
 
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-		}
-
-		@Override
-		protected void onPostExecute(TCPClient tcpClient) {
-			super.onPostExecute(tcpClient);
-		}
-
-		@Override
-		protected void onProgressUpdate(String... values) {
-
-		}
+  }
 
 
-		@Override
-		public void messageReceived(String message) {
-			publishProgress(message);
-			Log.d("TCP message", message);
-		}
+  private class TCPConnection extends AsyncTask<String, String, TCPClient> implements TCPClient.OnMessageReceived {
+    private String ipAddress;
+    private TCPClient client;
 
-		@Override
-		protected TCPClient doInBackground(String... params) {
-			client = new TCPClient(ipAddress, this, mTextConnectionStatus);
-			client.run();
-			return client;
-		}
-	}
+    public TCPConnection(String ipAddress) {
+      this.ipAddress = ipAddress;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+      super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(TCPClient tcpClient) {
+      super.onPostExecute(tcpClient);
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+
+    }
+
+
+    @Override
+    public void messageReceived(String message) {
+      publishProgress(message);
+      Log.d("TCP message", message);
+    }
+
+    @Override
+    protected TCPClient doInBackground(String... params) {
+      client = new TCPClient(ipAddress, this, mTextConnectionStatus);
+      client.run();
+      return client;
+    }
+  }
 
 }
--- Clase1/app/src/main/java/com/robotica/clase1/TCPClient.java
+++ Clase1/app/src/main/java/com/robotica/clase1/TCPClient.java
@@ -13,129 +13,129 @@ import java.net.Socket;
 
 public class TCPClient {
 
-	private String serverMessage;
-	private String ipAddress;
-	public static final int SERVER_PORT = 2000;
-	private OnMessageReceived mMessageListener = null;
-	private boolean mRun = false;
+  private String serverMessage;
+  private String ipAddress;
+  public static final int SERVER_PORT = 2000;
+  private OnMessageReceived mMessageListener = null;
+  private boolean mRun = false;
 
-	private PrintWriter out;
-	private BufferedReader in;
-	private Socket socket;
-	public boolean con;
-	public boolean con2;
+  private PrintWriter out;
+  private BufferedReader in;
+  private Socket socket;
+  public boolean con;
+  public boolean con2;
 
-	private TextView textView;
-
-
-	public TCPClient(String ipAddress, OnMessageReceived listener, TextView textView) {
-		this.ipAddress = ipAddress;
-		mMessageListener = listener;
-		this.textView = textView;
-	}
+  private TextView textView;
 
 
-	/**
-	 * Sends the message entered by client to the server
-	 *
-	 * @param message text entered by client
-	 */
-	public void sendMessage(String message) {
-		if (out != null && !out.checkError()) {
-			Log.d("Message sent: ", message);
-			out.println(message);
-			out.flush();
-		}
-	}
-
-	public String stopClient() throws Throwable {
-		mRun = false;
-		socket.close();
-		con2 = socket.isClosed();
-		if (con2) {
-			con = false;
-			//textView.setText("Desconectado");
-			return "*desconectado*";
-
-		}
-		return null;
-	}
-
-	public String conection() {
-
-		if (con) {
-			//textView.setText("Conectado");
-			return "¡conectado OK¡";
-		}
-		return "error en la conexion";
-	}
+  public TCPClient(String ipAddress, OnMessageReceived listener, TextView textView) {
+    this.ipAddress = ipAddress;
+    mMessageListener = listener;
+    this.textView = textView;
+  }
 
 
-	public void run() {
+  /**
+   * Sends the message entered by client to the server
+   *
+   * @param message text entered by client
+   */
+  public void sendMessage(String message) {
+    if (out != null && !out.checkError()) {
+      Log.d("Message sent: ", message);
+      out.println(message);
+      out.flush();
+    }
+  }
 
-		mRun = true;
+  public String stopClient() throws Throwable {
+    mRun = false;
+    socket.close();
+    con2 = socket.isClosed();
+    if (con2) {
+      con = false;
+      //textView.setText("Desconectado");
+      return "*desconectado*";
 
-		try {
-			//here you must put your computer's IP address.
-			InetAddress serverAddr = InetAddress.getByName(ipAddress);
+    }
+    return null;
+  }
 
-			Log.e("TCP Client", "C: Connecting...");
-			//create a socket to make the connection with the server
-			socket = new Socket(serverAddr, SERVER_PORT);
+  public String conection() {
 
-			try {
-
-				//send the message to the server
-				out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-
-				Log.i("TCP Client", "C: Sent.");
-				//_tv_estado.setText("Estado: Conectado");
-				Log.i("TCP Client", "C: Done.");
-				//textView.setText("Conectado");
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				if (socket.isConnected() == true) {
-					con = true;
-					//textView.setText("Conectado");
-				}
-				//in this while the client listens for the messages sent by the server
-				while (mRun) {
-					serverMessage = in.readLine();
-
-					if (serverMessage != null && mMessageListener != null) {
-						//call the method messageReceived from MyActivity class
-						mMessageListener.messageReceived(serverMessage);
-					}
-					serverMessage = null;
-
-				}
+    if (con) {
+      //textView.setText("Conectado");
+      return "¡conectado OK¡";
+    }
+    return "error en la conexion";
+  }
 
 
-				Log.i("RSPONSE FROM SERVER", "S: Received Message: '" + serverMessage + "'");
+  public void run() {
+
+    mRun = true;
+
+    try {
+      //here you must put your computer's IP address.
+      InetAddress serverAddr = InetAddress.getByName(ipAddress);
+
+      Log.e("TCP Client", "C: Connecting...");
+      //create a socket to make the connection with the server
+      socket = new Socket(serverAddr, SERVER_PORT);
+
+      try {
+
+        //send the message to the server
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+        Log.i("TCP Client", "C: Sent.");
+        //_tv_estado.setText("Estado: Conectado");
+        Log.i("TCP Client", "C: Done.");
+        //textView.setText("Conectado");
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        if (socket.isConnected() == true) {
+          con = true;
+          //textView.setText("Conectado");
+        }
+        //in this while the client listens for the messages sent by the server
+        while (mRun) {
+          serverMessage = in.readLine();
+
+          if (serverMessage != null && mMessageListener != null) {
+            //call the method messageReceived from MyActivity class
+            mMessageListener.messageReceived(serverMessage);
+          }
+          serverMessage = null;
+
+        }
 
 
-			} catch (Exception e) {
-
-				Log.e("TCP", "S: Error", e);
-
-			} finally {
-				//the socket must be closed. It is not possible to reconnect to this socket
-				// after it is closed, which means a new socket instance has to be created.
-				socket.close();
-				//textView.setText("Desconectado");
-			}
-
-		} catch (Exception e) {
-
-			Log.e("TCP", "C: Error", e);
-			//textView.setText("Error");
-		}
+        Log.i("RSPONSE FROM SERVER", "S: Received Message: '" + serverMessage + "'");
 
 
-	}
+      } catch (Exception e) {
 
-	//Declare the interface. The method messageReceived(String message) will must be implemented in the MyActivity
-	//class at on asynckTask doInBackground
-	public interface OnMessageReceived {
-		public void messageReceived(String message);
-	}
+        Log.e("TCP", "S: Error", e);
+
+      } finally {
+        //the socket must be closed. It is not possible to reconnect to this socket
+        // after it is closed, which means a new socket instance has to be created.
+        socket.close();
+        //textView.setText("Desconectado");
+      }
+
+    } catch (Exception e) {
+
+      Log.e("TCP", "C: Error", e);
+      //textView.setText("Error");
+    }
+
+
+  }
+
+  //Declare the interface. The method messageReceived(String message) will must be implemented in the MyActivity
+  //class at on asynckTask doInBackground
+  public interface OnMessageReceived {
+    public void messageReceived(String message);
+  }
 }
